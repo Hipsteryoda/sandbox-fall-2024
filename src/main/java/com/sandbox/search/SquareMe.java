d5cf42ef5e1c6249f60b468b4e0727863b104476
Hipsteryoda <ksmith952913@gmail.com>
2024-11-19 18:22:54 -0700

    syncing fork

--- src/main/java/com/sandbox/search/SquareMe.java
+++ src/main/java/com/sandbox/search/SquareMe.java
@@ -2,8 +2,15 @@ package com.sandbox.search;
 
 public class SquareMe {
 
+<<<<<<< HEAD
     public static void main(String[] args) {
 
         SquareRoot.calculateFor(25, 0.01);
     }
+=======
+  public static void main(String[] args) {
+    
+    SquareRoot.calculateFor(25);
+  }
+>>>>>>> 9f6c683 (Sorting activity)
 }
--- src/main/java/com/sandbox/sort/SortTest.java
+++ src/main/java/com/sandbox/sort/SortTest.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class SortTest {
 
+<<<<<<< HEAD
     public static void main(String[] args) {
 
         Integer[] arr = {5, 3, 1, 2, 4};
@@ -11,4 +12,37 @@ public class SortTest {
 
         Arrays.stream(arr).forEach(System.out::println);
     }
+=======
+  public static void main(String[] args) {
+    Integer[] data = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+
+    SortingUtility.selectionSort(data);
+
+    Arrays.stream(data).forEach(System.out::println);
+
+    Integer[] data2 = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+
+    SortingUtility.insertionSort(data2);
+
+    Arrays.stream(data2).forEach(System.out::println);
+
+    Integer[] data3 = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+
+    SortingUtility.bubbleSort(data3);
+
+    Arrays.stream(data3).forEach(System.out::println);
+
+    Integer[] data4 = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+
+    SortingUtility.quickSort(data4);
+
+    Arrays.stream(data4).forEach(System.out::println);
+
+    Integer[] data5 = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5 };
+
+    SortingUtility.mergeSort(data5);
+
+    Arrays.stream(data5).forEach(System.out::println);
+  }
+>>>>>>> 9f6c683 (Sorting activity)
 }
